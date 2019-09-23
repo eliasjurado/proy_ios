65b3fca922d3ba9d1fda5973309644c8158c44fd
Elías Luis Jurado Santos <eliasljurado@gmail.com>
2019-09-23 10:45:10 -0700

    Creación del Proyecto de IOS
    Debemos definir el tema y las vistas

---
+++ 02_Fuente/Proy IOS/Proy IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proy IOS
+//
+//  Created by P on 9/23/19.
+//  Copyright © 2019 JSTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
